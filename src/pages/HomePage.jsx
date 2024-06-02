4944b9220efd1c429e7a316ffa47c725bf9cdf8d
Victoria Ebong <79677353+victoriaEssien@users.noreply.github.com>
2024-06-02 20:10:33 +0100

    api error fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ function HomePage() {
   return (
     <div>
         <h1>HomePage</h1>
-        <a href="/login" className="bg-primary-700 hover:bg-primary-700 px-4 py-3.5">Back to login</a>
+        <a href="/login" className="block w-2/6 bg-primary-700 hover:bg-primary-700 px-4 py-3.5">Back to login</a>
     </div>
   )
 }
