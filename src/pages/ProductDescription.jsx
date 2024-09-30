293cc55b32a762a2173aae880813c488afaedce0
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-09-30 22:17:39 +0100

    Putting the product screen together

--- src/pages/ProductDescription.jsx
+++ src/pages/ProductDescription.jsx
@@ -6,6 +6,10 @@ import AppNav from '../components/AppNav';
 // Placeholder images
 import ToteBag from "../assets/images/tote-bag.png";
 
+// React icons
+import { FaRegHeart } from "react-icons/fa6"; // Favourite icon
+import { FaStar } from "react-icons/fa"; // Star icon
+
 function ProductDescription() {
 
   return (
@@ -13,7 +17,9 @@ function ProductDescription() {
       <AppNav />
 
       <div className='mt-8 md:mx-8'>
+
         <div>
+
           <div>
             <img src={ ToteBag } alt='Image loading...' className='mx-auto md:mx-0' />
             <div className='flex my-3 justify-center'>
@@ -24,8 +30,24 @@ function ProductDescription() {
               <img src={ ToteBag } alt='Image loading...' className='w-12 mx-1' />
             </div>
           </div>
-          <div></div>
+
+          <div className='mx-4 mt-8'>
+            <p className='bg-blue-100 inline-block p-1 px-3 rounded text-xs'>JaneDeo shop</p>
+            <div className='flex'>
+              <h4 className='mt-3 text-xl'>5 Pairs Quality Cotton Ankle Socks - White&Black&Navy&Gray</h4>
+              <FaRegHeart className='text-2xl cursor-pointer' />
+            </div>
+            <p className='my-3 text-green-600'>In stock</p>
+            <h4 className='text-2xl font-normal'>₦4,500</h4>
+            <hr className='my-3' />
+
+            <p className='mt-1 font-thin'>Variation Available</p>
+
+            <button className='p-2 border rounded my-3'>ONE SIZE FITS ALL</button>
+          </div>
+
         </div>
+
       </div>
 
     </div>
