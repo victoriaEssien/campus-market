6f36d241066599755b5e73cd8632fca74256e604
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-09-30 20:29:14 +0100

    Installed some few things like React-icons

--- src/pages/ProductDescription.jsx
+++ src/pages/ProductDescription.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 
 // Components
-import AppNav from '../components/AppNav'
+import AppNav from '../components/AppNav';
+
+// Placeholder images
+import ToteBag from "../assets/images/tote-bag.png";
 
 function ProductDescription() {
+
   return (
     <div>
       <AppNav />
+
+      <div className='mt-8 md:mx-8'>
+        <div>
+          <div>
+            <img src={ ToteBag } alt='Image loading...' className='mx-auto md:mx-0' />
+            <div className='flex my-3 justify-center'>
+              <img src={ ToteBag } alt='Image loading...' className='w-12 mx-1' />
+              <img src={ ToteBag } alt='Image loading...' className='w-12 mx-1' />
+              <img src={ ToteBag } alt='Image loading...' className='w-12 mx-1' />
+              <img src={ ToteBag } alt='Image loading...' className='w-12 mx-1' />
+              <img src={ ToteBag } alt='Image loading...' className='w-12 mx-1' />
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
+
     </div>
   )
 }
